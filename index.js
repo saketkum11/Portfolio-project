8bbd2a73c1ef0b459f532bdc2aca5c183eb6096b
Saket kumar <kumarsaket601@gmail.com>
2022-12-07 21:00:19 +0530

    css and html

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const saket = "hello";
-if (saket === "hello") {
-  console.log("true");
+var navBtn = document.querySelector("#nav_button");
+var sideNav = document.querySelector(".side_nav");
+var closeBtn = document.querySelector(".cross_icon_btn");
+var crossIcon = document.querySelector(".cross_icon");
+
+navBtn.addEventListener("click", openSideNavigation);
+closeBtn.addEventListener("click", closeNavigation);
+
+function openSideNavigation() {
+  sideNav.classList.remove("side_nav");
+}
+function closeNavigation() {
+  crossIcon.classList.add("side_nav");
 }
